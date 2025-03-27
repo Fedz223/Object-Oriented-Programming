d30c1555dca1fc61df91650482bec9327b438de7
Federix <fedza001@gmail.com>
2025-03-27 21:37:34 +0700

    Add files via upload

---
+++ Database1.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package database1;
+
+/**
+ *
+ * @author Fedz
+ */
+public class Database1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
